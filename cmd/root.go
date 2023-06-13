737f2b917a95e5cd687f035f36288069f620d292
docktermj <michael@dockter.com>
2023-06-13 15:56:20 -0400

    #52 Savepoint

--- cmd/root.go
+++ cmd/root.go
@@ -111,7 +111,7 @@ func PreRun(cobraCommand *cobra.Command, args []string) {
 // Used in construction of cobra.Command
 func RunE(_ *cobra.Command, _ []string) error {
 	var err error = nil
-	ctx := context.TODO()
+	ctx := context.Background()
 	examplePackage := &examplepackage.ExamplePackageImpl{
 		Something: "Main says 'Hi!'",
 	}
