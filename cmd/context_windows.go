48af2268bc213adbd6efdabf35710e4f78ff9724
docktermj <michael@dockter.com>
2023-07-13 15:30:12 -0400

    #57 Support platform-specific context variables

---
+++ cmd/context_windows.go
@@ -0,0 +1,13 @@
+//go:build windows
+
+package cmd
+
+import (
+	"github.com/senzing/senzing-tools/cmdhelper"
+)
+
+var ContextBoolsForOsArch = []cmdhelper.ContextBool{}
+
+var ContextIntsForForOsArch = []cmdhelper.ContextInt{}
+
+var ContextStringsForOsArch = []cmdhelper.ContextString{}
